65c8850971802618af00fdb77f2e6f368d21409a
Toodle <alicebobo223@gmai.com>
2022-12-05 23:02:03 +0800

    臨時停車請走左邊，本校教職員、學生請走右邊 機車限速10公里，請減速慢行 本出入口為緊急逃生使用，請改由一號出入口進出停車場 車牌辨識中，請保持一個車身的距離 請減速慢行，禮讓行人

--- 111-1QZ4/Test.aspx.cs
+++ 111-1QZ4/Test.aspx.cs
@@ -1,30 +1,17 @@
-﻿# 第4次隨堂-隨堂-QZ4
->
->學號：109111103
-><br />
->姓名：曾昱翔
-><br />
->作業撰寫時間：80 (mins，包含程式撰寫時間)
-><br />
->最後撰寫文件日期：2022/12/5
->
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Runtime.Remoting.Contexts;
 
-本份文件包含以下主題：(至少需下面兩項，若是有多者可以自行新增)
-- [x]說明內容
-- [x]個人認為完成作業須具備觀念
 
-## 說明程式與內容
-
-開始寫說明，該說明需說明想法，
-並於之後再對上述想法的每一部分將程式進一步進行展現，
-若需引用程式區則使用下面方法，
-若為.cs檔內程式除了於敘述中需註明檔案名稱外，
-還需使用語法` ```csharp 程式碼 ``` `，
-下段程式碼則為使用後結果：
-
-```csharp
-建立Test連接字串的區域變數
- public partial class Test : System.Web.UI.Page
+namespace _111_1QZ4
+{
+    public partial class Test : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -38,7 +25,8 @@
                 "ApplicationIntent=ReadWrite;MultiSubnetFailover=False;" +
                 "User ID = sa; Password = 12345";
 
-顯示出dbo.Users裡的使用者和資料
+
+
             try
             {
                 SqlConnection o_Conn = new SqlConnection(s_ConnS);
@@ -62,14 +50,3 @@
         }
     }
 }
-```
-
-
-## 個人認為完成作業須具備觀念
-
-開始寫說明，需要說明本次作業個人覺得需學會那些觀念 (需寫成文章，需最少50字，
-並且文內不得有你、我、他三種文字)
-
-此次的隋棠很需要跟sql有關的語法和觀念，否則很難做出來，
-不但要知道怎麼讓程式去抓資料，還要知道怎麼直接在sql中操作。
-
